15e18e889bc2b017ccbc930bf708447d30968879
ischo <ischo@LAPTOP-9GOA4UHJ.localdomain>
2023-12-22 14:59:37 +0800

    incorporated sign in, signup

--- src/routes/users/login/+page.svelte
+++ src/routes/users/login/+page.svelte
@@ -1,79 +1,66 @@
 <script>
-    import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public';
-    import { goto } from '$app/navigation';
+  import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public';
+  import { goto } from '$app/navigation';
+  import { authenticateUser, isValidToken } from '../../../utils/auth';
 
-    let formErrors = {};
-  
-    async function signInUser(evt) {
-      evt.preventDefault();
-  
-      const userData = {
-        email: evt.target['email'].value,
-        password: evt.target['password'].value,
-      };
-  
-      const resp = await fetch(PUBLIC_BACKEND_BASE_URL + '/auth', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(userData)
-      });
-  
-      console.log(resp)
-  
-      if (resp.status === 200) {
-        const { userId, accessToken } = await resp.json();
-        localStorage.setItem('userId', userId);
-        localStorage.setItem('accessToken', accessToken, );
+  let formErrors = {};
 
-      } else {
-        const res = await resp.json();
-        console.error('Sign in failed:', res);
-        if (res.error) {
-          formErrors = res.error;
-        } else {
-          console.error('Sign in failed:', res);
-        }
-      }
+  function postLogIn(){
+    isValidToken();
+    goto('/')
+  }
+
+  async function logIn(evt) {
+    evt.preventDefault();
+
+    const userData = {
+      email: evt.target['email'].value,
+      password: evt.target['password'].value,
+    };
+
+    const resp = await authenticateUser(userData.email, userData.password);
+
+    if(resp.success){
+      postLogIn();
+    }else{
+      alert("Invalid email or password.")
     }
-  </script>
-  
-  <br>
-  <h1 class="text-center text-xl">Login to Your Account</h1>
-  <div class="text-center">
-    <a class="link-hover italic text-xs" href="/users/signUp">Don't have an account? Click here to sign up.</a>
-  </div>
-  <div class="flex justify-center items-center mt-8">
-    <form on:submit={signInUser} class="w-1/3">
-      <div class="form-control w-full">
-        <label class="label" for="email">
-          <span class="label-text">Email</span>
-        </label>
-        <input type="text" name="email" placeholder="johndoe@example.com" class="input input-bordered w-full" />
-        {#if 'email' in formErrors}
-        <label class="label" for="email">
-          <span class="label-text-alt text-red-500">{formErrors['email']}</span>
-        </label>
-        {/if}
-      </div>
-  
-      <div class="form-control w-full">
-        <label class="label" for="password">
-          <span class="label-text">Password</span>
-        </label>
-        <input type="password" name="password" placeholder="" class="input input-bordered w-full" required />
-        {#if 'password' in formErrors}
-        <label class="label" for="password">
-          <span class="label-text-alt text-red-500">{formErrors['password']}</span>
-        </label>
-        {/if}
-      </div>
-  
-      <div class="form-control w-full mt-4">
-        <button class="btn btn-md bg-blue-500 text-white">Sign In</button>
-      </div>
-    </form>
-  </div>
-  
+  }
+</script>
+
+<br>
+<h1 class="text-center text-xl">Login to Your Account</h1>
+<div class="text-center">
+  <a class="link-hover italic text-xs" href="/users/signUp">Don't have an account? Click here to sign up.</a>
+</div>
+<div class="flex justify-center items-center mt-8">
+  <form on:submit={logIn} class="w-1/3">
+    <div class="form-control w-full">
+      <label class="label" for="email">
+        <span class="label-text">Email</span>
+      </label>
+      <input type="text" name="email" placeholder="johndoe@example.com" class="input input-bordered w-full" />
+      {#if 'email' in formErrors}
+      <label class="label" for="email">
+        <span class="label-text-alt text-red-500">{formErrors['email']}</span>
+      </label>
+      {/if}
+    </div>
+
+    <div class="form-control w-full">
+      <label class="label" for="password">
+        <span class="label-text">Password</span>
+      </label>
+      <input type="password" name="password" placeholder="" class="input input-bordered w-full" required />
+      {#if 'password' in formErrors}
+      <label class="label" for="password">
+        <span class="label-text-alt text-red-500">{formErrors['password']}</span>
+      </label>
+      {/if}
+    </div>
+
+    <div class="form-control w-full mt-4">
+      <button class="btn btn-md bg-blue-500 text-white">Sign In</button>
+    </div>
+  </form>
+</div>
